4a57e6d290c592294abb3b11f6773e49530246a2
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-08-18 22:25:36 -0500

    Manipulated the dom

--- 4-DOM_manipulation/logic.js
+++ 4-DOM_manipulation/logic.js
@@ -15,13 +15,16 @@ var scores, roundScores, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
-var rolls = [];
+console.log(dice);
 
-for (var i = 0; i < 10; i++) {
-    rolls.push(dice);
-}
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-console.log(rolls);
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
