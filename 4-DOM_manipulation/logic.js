e655c8bf780aef04bc6fb9e961f06b83be607aaf
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-08-20 15:09:05 -0500

    Challenge 1 slain

--- 4-DOM_manipulation/logic.js
+++ 4-DOM_manipulation/logic.js
@@ -7,7 +7,9 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 //Global Variables
-var scores, roundScores, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying;
+
+var rolls = [];
 
 startGame();
 
@@ -17,12 +19,31 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         //1. Generate a random number between 1 and six and store it as a variable.
         var dice = Math.floor(Math.random() * 6) + 1;
 
+        //Push the result of each roll into the 'rolls' array
+        rolls.push(dice);
+        
+        //Once the array reaches a length of two, cap it by shifting off the first item for every roll thereafter
+        if (rolls.length > 2) {
+            rolls.shift();
+        }
+        
+        if (rolls[0] === 6 && rolls[1] === 6) {
+            //zero out all active player scores
+            roundScore = 0;
+            scores[activePlayer] = 0;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+            nextPlayer();
+        };
+
         //2. Display the result
         var diceEl = document.querySelector('.dice');
         diceEl.style.display = 'block';
         diceEl.src = 'dice-' + dice + '.png';
 
-        //3. Update the round score IF it was not a 1
+        console.log(rolls);
+
+        //3. Update the round score IF it was not a 1 OR if the last two rolls were not sixes
         if (dice !== 1) {
             //Add Score
             roundScore += dice;
@@ -95,4 +116,17 @@ function startGame(){
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 };
+
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn.
+ (Hint: Always save the previous dice roll in a separate variable)
+
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
+
+
 
