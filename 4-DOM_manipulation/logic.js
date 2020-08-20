3106d4978a99b0f22a0c65403600c3f8269a1f9d
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-08-19 20:07:47 -0500

    Game finished!

--- 4-DOM_manipulation/logic.js
+++ 4-DOM_manipulation/logic.js
@@ -1,58 +1,60 @@
 /*
 GAME RULES:
-
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 //Global Variables
-var scores, roundScores, activePlayer;
+var scores, roundScores, activePlayer, gamePlaying;
 
 startGame();
 
-document.querySelector('.btn-roll').addEventListener('click', roll);
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    if(gamePlaying) {
+        console.log(`Roll, homie!`);
+        //1. Generate a random number between 1 and six and store it as a variable.
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-function roll() {
-    console.log(`Roll, homie!`);
-    //1. Generate a random number between 1 and six and store it as a variable.
-    var dice = Math.floor(Math.random() * 6) + 1;
+        //2. Display the result
+        var diceEl = document.querySelector('.dice');
+        diceEl.style.display = 'block';
+        diceEl.src = 'dice-' + dice + '.png';
 
-    //2. Display the result
-    var diceEl = document.querySelector('.dice');
-    diceEl.style.display = 'block';
-    diceEl.src = 'dice-' + dice + '.png';
-
-    //3. Update the round score IF it was not a 1
-    if (dice !== 1) {
-        //Add Score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        //Next player
-        nextPlayer();
+        //3. Update the round score IF it was not a 1
+        if (dice !== 1) {
+            //Add Score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
+        }
     }
-}
+});
+
+
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-    // Add CURRENT score to GLOBAL score
-    scores[activePlayer] += roundScore;
+    if (gamePlaying) {
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
 
-    //Update the UI
-    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]; 
-    //Check if the player won the game
-    if (scores[activePlayer] >= 100) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-        } else {
+        //Update the UI
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]; 
+        //Check if the player won the game
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+            } else {
             //The next player takes their turn
             nextPlayer();
         };
-
+    }    
 });
 
 function nextPlayer() {
@@ -74,6 +76,7 @@ function nextPlayer() {
 document.querySelector('.btn-new').addEventListener('click', startGame);
 
 function startGame(){
+    gamePlaying = true;
     scores = [0, 0];
     activePlayer = 0;
     roundScore = 0;
@@ -92,3 +95,4 @@ function startGame(){
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 };
+
