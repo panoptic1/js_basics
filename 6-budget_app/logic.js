9cd50d770ea83b39aa7f32b78e2b59c6cb7421ea
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-09-09 21:28:53 -0500

    The module pattern

--- 6-budget_app/logic.js
+++ 6-budget_app/logic.js
@@ -1,1 +1,18 @@
 console.log("Let's organize our finances!");
+
+//the module pattern
+//(uses an IIFE)
+
+var budgetController = (function() {
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            console.log(add(b));
+        }
+    }
+})();
