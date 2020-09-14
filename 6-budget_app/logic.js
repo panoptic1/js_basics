f777c1d0b526e622ce8075e91a20db6da294e624
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-09-14 18:28:20 -0500

    Added budget calculations

--- 6-budget_app/logic.js
+++ 6-budget_app/logic.js
@@ -20,6 +20,14 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(current) {
+            sum += current.value;
+        });
+        data.totals[type] = sum;
+    };
+
     var data = {
 
         allItems: {
@@ -30,7 +38,10 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -54,6 +65,33 @@ var budgetController = (function() {
             //Push it into our data structure
             data.allItems[type].push(newItem);
             return newItem;
+        },
+
+        calculateBudget: function() {
+
+            // calculate total income and expenses
+            calculateTotal(`exp`);
+            calculateTotal(`inc`);
+
+            // calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // calculate the percentage of income that we spent
+            if (data.totals.inc > 0){
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function() {
@@ -167,10 +205,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var updateBudget = function() {
         // 1. calculate the budget
+        budgetController.calculateBudget();
 
         // 2. return the budget
+        var budget = budgetController.getBudget();
 
         // 3. display the budget
+        console.log(budget);
     }
 
     var ctrlAddItem = function() {
