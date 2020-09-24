51c40a7b5e91f19b24c1e270f30e574ce9712b7a
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-09-24 13:58:40 -0500

    Arrow functions 1

--- 7-ES6/logic.js
+++ 7-ES6/logic.js
@@ -121,3 +121,28 @@ console.log(fullName.includes('an He'));
 
 //'startsWith and endsWith allows us to check whether or not a string starts or ends with something
 console.log(`${firstName} `.repeat(5));
+
+//Lecture: ARROW FUNCTIONS
+
+const years = [1990, 1965, 1982, 1937];
+
+//ES5
+var ages5 = years.map(function(el){
+    return 2020 - el;
+});
+
+console.log(ages5);
+
+//ES6
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1} : ${age}.`
+});
+console.log(ages6);
