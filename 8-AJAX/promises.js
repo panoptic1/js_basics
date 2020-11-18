e64b379467d0fce0d82f86a59653f62d54d4980c
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-11-17 19:31:04 -0600

    Promises made.

---
+++ 8-AJAX/promises.js
@@ -0,0 +1,41 @@
+console.log("#434-Wherein I learn about promises.");
+
+const getIDs = new Promise ((resolve, reject) => { //the function planted inside of the Promise is called 'the executor'. Pretty badass. 
+    setTimeout(() => {
+        resolve([13, 27, 43, 102, 265]);
+    }, 1500);
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = {title: `Grilled Mushroom Tacos`, publisher: `Ryan`};
+            resolve(`${ID} : ${recipe.title} by ${recipe.publisher}`);
+        }, 1500, recID);
+    });
+};
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout(pub => {
+            const recipe = {title: `Italian Beanballs`, publisher: `Ryan`};
+                resolve(`Also by ${pub} : ${recipe.title}`);
+        }, 1500, publisher);
+    })
+};
+
+getIDs
+.then(IDs => {
+    console.log(IDs);
+    return getRecipe(IDs[2])
+})
+.then(recipe => {
+    console.log(recipe);
+    return getRelated('Ryan');
+})
+.then(recipe => {
+    console.log(recipe);
+})
+.catch(error => {
+    console.log(error);
+})
