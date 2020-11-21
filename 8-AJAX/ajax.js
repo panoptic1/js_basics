ad230a3e982881b9fd41ea2fb75099827a996480
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-11-21 10:11:47 -0600

    AJAX calls with Fetch + Async/Await

--- 8-AJAX/ajax.js
+++ 8-AJAX/ajax.js
@@ -1,12 +1,40 @@
 console.log(`#437-Making AJAX calls.`);
 
-fetch('https://api.tronalddump.io/random/quote')
+
+function getDumbassQuote() {
+    fetch('https://api.tronalddump.io/random/quote')
 .then(response => {
-    console.log(response);
+    //console.log(response);
     return response.json(); //I struggled with this for a moment as I had forgotten about having to use this method to convert the data
 })
 .then(data => {
-    console.log(data);
-    console.log(data.value);
+    //console.log(data);
+    console.log(`A random dumbass thing that was said by the orange president: "${data.value}" Seriously, what a dumbass.`);
 })
 .catch(error => console.log(error));
+
+}
+
+getDumbassQuote();
+
+async function getDumbassQuoteAS(){
+    const result = await fetch('https://api.tronalddump.io/random/quote');
+    const data = await result.json();
+    console.log(data);
+    console.log(`Here is an asychronous dumbass quote: "${data.value}"`)
+    return data;
+}
+
+getDumbassQuoteAS()
+
+//if you want to get and store the value of the async function in the global scope, this will not work:
+const dumbassQuote = getDumbassQuoteAS();
+console.log(dumbassQuote);
+// the reason for that is the reason why the log statement on line 32 makes it to the console before the statements
+// on lines 23 and 24: the log statement on line 32 is executed before the value has been returned by the asynchronous function
+let asyncDummy;
+getDumbassQuoteAS().then(data => {
+    asyncDummy = data;
+    console.log(asyncDummy);
+});
+
